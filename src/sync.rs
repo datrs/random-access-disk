35bbeb3f26c4fd777e97d5c79890f75bb74db54b
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-25 14:31:37 +1100

    accept a path

--- src/sync.rs
+++ src/sync.rs
@@ -1,14 +1,17 @@
 extern crate failure;
+extern crate mkdirp;
 extern crate random_access_storage as random_access;
 
 use failure::Error;
+use std::path::Path;
 
 /// Main constructor.
-pub struct Sync;
+pub struct Sync {}
+
 impl Sync {
   /// Create a new instance.
-  pub fn new() -> random_access::Sync<SyncMethods> {
-    let methods = SyncMethods {};
+  pub fn new<'f>(filename: &'f Path) -> random_access::Sync<SyncMethods> {
+    let methods = SyncMethods { filename: filename };
 
     random_access::Sync::new(methods)
   }
@@ -17,10 +20,13 @@ impl Sync {
 /// Methods that have been implemented to provide synchronous access to disk.  .
 /// These should generally be kept private, but exposed to prevent leaking
 /// internals.
-pub struct SyncMethods {}
+pub struct SyncMethods<'f> {
+  pub filename: &'f Path,
+}
 
-impl random_access::SyncMethods for SyncMethods {
+impl<'f> random_access::SyncMethods for SyncMethods<'f> {
   fn open(&mut self) -> Result<(), Error> {
+    mkdirp::mkdirp(&self.filename);
     Ok(())
   }
 
