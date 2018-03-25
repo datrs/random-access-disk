dd4f667320de93e40bd3fe6cc90b148e8a5ca433
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-25 21:39:35 +1100

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,19 @@
 // #![cfg_attr(test, plugin(clippy))]
 
 //! Continuously read,write to disk, using random offsets and lengths.
+//!
+//! ```rust,ignore
+//! extern crate random_access_disk;
+//!
+//! use std::path::PathBuf;
+//!
+//! let dir = TempDir::new("random-access-disk").unwrap();
+//! let mut file = rad::Sync::new(PathBuf::from("./file.log"));
+//!
+//! file.write(0, b"hello")?;
+//! file.write(5, b" world")?;
+//! let text = file.read(0, 11)?;
+//! ```
 
 // #[macro_use]
 extern crate failure;
