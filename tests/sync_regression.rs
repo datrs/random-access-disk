12c93184eebd267ea544114775273a13a8d9e890
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-05-08 20:25:37 +0200

    fix regression

--- tests/sync_regression.rs
+++ tests/sync_regression.rs
@@ -2,6 +2,7 @@ extern crate random_access_disk as rad;
 extern crate tempdir;
 
 use self::tempdir::TempDir;
+use std::env;
 
 #[test]
 // postmortem: read_exact wasn't behaving like we hoped, so we had to switch
@@ -11,4 +12,16 @@ fn regress_1() {
   let mut file = rad::Sync::new(dir.path().join("regression-1.db"));
   file.write(27, b"").unwrap();
   file.read(13, 5).unwrap();
+}
+
+#[test]
+// postmortem: accessing the same file twice would fail, so we had to switch to
+// from `.create_new()` to `.create()`.
+//
+// NOTE: test needs to be run twice in a row to trigger regression. I'm sorry.
+fn regress_2() {
+  let mut dir = env::temp_dir();
+  dir.push("regression-2.db");
+  let mut file = rad::Sync::new(dir);
+  file.write(27, b"").unwrap();
 }
