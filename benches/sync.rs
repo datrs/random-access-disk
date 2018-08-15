cd61911a942379b000dae4b514e98ce7af6cda03
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2018-08-15 13:13:50 +0200

    Fix benches
    Signed-off-by: Szabolcs Berecz <szabolcs.berecz@gmail.com>

--- benches/sync.rs
+++ benches/sync.rs
@@ -2,16 +2,18 @@
 
 mod sync {
   extern crate random_access_disk as rad;
-  extern crate tempdir;
+  extern crate tempfile;
   extern crate test;
 
-  use self::tempdir::TempDir;
   use self::test::Bencher;
 
   #[bench]
   fn write_hello_world(b: &mut Bencher) {
-    let dir = TempDir::new("random-access-disk").unwrap();
-    let mut file = rad::Sync::new(dir.path().join("1.db"));
+    let dir = tempfile::Builder::new()
+      .prefix("random-access-disk")
+      .tempdir()
+      .unwrap();
+    let mut file = rad::RandomAccessDisk::new(dir.path().join("1.db"));
     b.iter(|| {
       file.write(0, b"hello").unwrap();
       file.write(5, b" world").unwrap();
@@ -20,8 +22,11 @@ mod sync {
 
   #[bench]
   fn read_hello_world(b: &mut Bencher) {
-    let dir = TempDir::new("random-access-disk").unwrap();
-    let mut file = rad::Sync::new(dir.path().join("2.db"));
+    let dir = tempfile::Builder::new()
+      .prefix("random-access-disk")
+      .tempdir()
+      .unwrap();
+    let mut file = rad::RandomAccessDisk::new(dir.path().join("2.db"));
     file.write(0, b"hello").unwrap();
     file.write(5, b" world").unwrap();
     b.iter(|| {
@@ -31,8 +36,11 @@ mod sync {
 
   #[bench]
   fn read_write_hello_world(b: &mut Bencher) {
-    let dir = TempDir::new("random-access-disk").unwrap();
-    let mut file = rad::Sync::new(dir.path().join("3.db"));
+    let dir = tempfile::Builder::new()
+      .prefix("random-access-disk")
+      .tempdir()
+      .unwrap();
+    let mut file = rad::RandomAccessDisk::new(dir.path().join("3.db"));
     b.iter(|| {
       file.write(0, b"hello").unwrap();
       file.write(5, b" world").unwrap();
