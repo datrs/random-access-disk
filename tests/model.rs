a7505301f521abbe746f4466e8b423d8659406f9
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-09-17 16:54:38 +0200

    fix quickcheck tests
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- tests/model.rs
+++ tests/model.rs
@@ -1,11 +1,13 @@
 #[macro_use]
 extern crate quickcheck;
+extern crate rand;
 extern crate random_access_disk as rad;
 extern crate random_access_storage;
 extern crate tempfile;
 
 use self::Op::*;
 use quickcheck::{Arbitrary, Gen};
+use rand::Rng;
 use random_access_storage::RandomAccess;
 use std::u8;
 use tempfile::Builder;
